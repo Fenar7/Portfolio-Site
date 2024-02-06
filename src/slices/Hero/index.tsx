1caccc69b5109b2340c3ddea4a9111efe459828c
Fenar Khan <fenarkhan@gmail.com>
2024-02-06 15:21:38 +0530

    jdfkjjsdfj

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -18,10 +18,10 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
           <div className="col-start-1 md:row-start-1">
             <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.firstt_name + " " + slice.primary.last_name}>
-              <span className="text-red-500">{slice.primary.firstt_name}</span>
-              <>{slice.primary.last_name}</>
-              <>{slice.primary.tag_line}</>
+              <span className="block text-slate-300">{slice.primary.firstt_name}</span>
+              <span className="-mt-[.2em] block">{slice.primary.last_name}</span>
             </h1>
+            <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2x1 font-bold uppercase tracking-[.2em] text-transparent opacity-1 md:text-4x1">{slice.primary.tag_line}</span>
           </div>
       </div>
     </section>
