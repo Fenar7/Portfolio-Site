d94eff04e70374f19280d319947ef39f0d11745b
Fenar Khan <fenarkhan@gmail.com>
2024-02-06 17:08:28 +0530

    jdfkjjsdfjdfsdf

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,5 +1,9 @@
+//to use js which runs on the client component 
+"use client";
+import { useEffect, useRef } from "react";
 import { Content,KeyTextField } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import gsap from 'gsap';
 
 /**
  * Props for `Hero`.
@@ -10,6 +14,26 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+  //gsap copmponent
+  const component = useRef(null);
+
+  useEffect(() => {
+    //animation goes here
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline()
+      //the first argument is the from state and the other is the to state
+      tl.fromTo(".name-animation",{
+        x: -100, opacity: 0, rotate: -10
+      },
+      {
+        x: 0, opacity: 1, rotate: 0
+      }
+      )
+
+    },component);
+    return () => ctx.revert();
+  }, [])
 
   const renderLetters = (name:KeyTextField, key:string) => {
     if(!name) return;
@@ -28,8 +52,8 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
           <div className="col-start-1 md:row-start-1">
             <h1 className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter" aria-label={slice.primary.firstt_name + " " + slice.primary.last_name}>
-              <span className="block text-slate-300">{slice.primary.firstt_name}</span>
-              <span className="-mt-[.2em] block">{slice.primary.last_name}</span>
+              <span className="block text-slate-300">{renderLetters(slice.primary.firstt_name,"first")}</span>
+              <span className="-mt-[.2em] block">{renderLetters(slice.primary.last_name,"last")}</span>
             </h1>
             <span className="block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2x1 font-bold uppercase tracking-[.2em] text-transparent opacity-1 md:text-4x1">{slice.primary.tag_line}</span>
           </div>
